f496d799c32add0af772e11644d719896386550d
chenxin <weilai995885@gmail.com>
2023-12-18 14:16:31 +0800

    订单状态定时处理

--- sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -6,6 +6,9 @@ import com.sky.entity.Orders;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Mapper
 public interface OrderMapper {
 
@@ -46,4 +49,14 @@ public interface OrderMapper {
      */
     @Select("select count(id) from orders where status = #{status}")
     Integer countStatus(Integer status);
+
+
+    /**
+     * 根据状态和时间查询超时订单
+     * @param status
+     * @param orderTime
+     * @return
+     */
+    @Select(("select *from orders where status = #{status} and order_time = #{orderTime}"))
+    List<Orders> getByStatusAndOrderTime(Integer status, LocalDateTime orderTime);
 }
